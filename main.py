010b13d62253608c4d3fd021dada51a241215939
Erik Schauer <do1ffe@darc.de>
2020-12-13 02:50:46 +0100

    Update

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ global comport
 ver = "0.9-alpha (GUI)"
 root = Tk()
 root.title("PTT v"+ver)
-root.geometry("600x400")
+root.geometry("600x200")
 root.iconbitmap('ptt.ico')
+root.config(bg="grey")
 icon = ImageTk.PhotoImage(Image.open('ptt.png').resize((100, 100)))
 fontStyle = tkFont.Font(family="Lucida Grande", size=18)
 
-label1 = Label(root, image=icon)
+label1 = Label(root, image=icon, bg="grey")
 label1.grid(row=0, column=0)
-label2 = Label(root, text="PTT v"+ver+"\n\xa9 12/2020 by Erik Schauer, DO1FFE", font=fontStyle)
-label2.grid(row=0, column=1, columnspan=3)
+label2 = Label(root, text="PTT v"+ver+"\n\xa9 12/2020 by Erik Schauer, DO1FFE", font=fontStyle, bg="grey")
+label2.grid(row=0, column=1, columnspan=4)
 
 OptionList = [
     "COM1",
@@ -30,21 +31,35 @@ OptionList = [
 
 def com_select(e):
     global ser
+    global comport
     comport = com_combo.get()
     ser = serial.Serial(comport)
     com_label.config(text=f"{comport}, 9600,8,N,1")
-    tx_button.config(text=f"{comport} TX")
+    com_combo.config(state=DISABLED)
+    tx_button.config(text=f"{comport} TX", state=ACTIVE)
+    close_com.config(state=ACTIVE)
 
 def senden():
     ser.setRTS(True)
     ser.setDTR(True)
-    print("TX")
+    tx_button.config(state=DISABLED)
+    rx_button.config(state=ACTIVE)
 
 def nicht_senden():
     ser.setRTS(False)
     ser.setDTR(False)
-    print("RX")
+    rx_button.config(state=DISABLED)
+    tx_button.config(state=ACTIVE)
 
+def com_schliessen():
+    rx_button.config(state=DISABLED)
+    tx_button.config(state=DISABLED)
+    close_com.config(state=DISABLED)
+    com_combo.config(state=ACTIVE)
+    com_label.config(text="<-- COM wählen!")
+    ser.setRTS(False)
+    ser.setDTR(False)
+    ser.close()
 
 com_combo = ttk.Combobox(root, value=OptionList)
 com_combo.config(width=7, font=('Helvetica', 12))
@@ -55,12 +70,14 @@ com_label = Label(root, text="<-- COM wählen!")
 com_label.config(width=20, font=('Helvetica', 12))
 com_label.grid(row=1, column=1)
 
-tx_button = Button(root, text="NO TX", command=senden)
+tx_button = Button(root, text="NO TX", state=DISABLED, command=senden)
 tx_button.grid(row=1, column=3)
 
-rx_button = Button(root, text="TX AUS", command=nicht_senden)
+rx_button = Button(root, text="TX AUS", state=DISABLED, command=nicht_senden)
 rx_button.grid(row=1, column=4)
 
+close_com = Button(root, text="COM-Port schliessen", state=DISABLED, command=com_schliessen)
+close_com.grid(row=2, column=0)
 
 '''
 print("###########################################################")
@@ -96,6 +113,6 @@ print("Programm beendet...")
 time.sleep(3)
 '''
 root.mainloop()
-# ser.setRTS(False)
-# ser.setDTR(False)
-# ser.close()
+ser.setRTS(False)
+ser.setDTR(False)
+ser.close()
